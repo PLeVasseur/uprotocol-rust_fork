3917b56d2274b65594a48cf6d0421c52e62d0111
Peter LeVasseur <plevasseur@gmail.com>
2023-12-25 14:41:05 -0500

    Wrote in entitybuilder. Began authoritybuilder.

--- src/uri/builder/entitybuilder.rs
+++ src/uri/builder/entitybuilder.rs
@@ -13,24 +13,65 @@
 
 use crate::uprotocol::{UEntity};
 
+enum EntityResolution {
+    Incomplete(UEntity),
+    MicroUri(UEntity),
+    LongUri(UEntity),
+    Resolved(UEntity),
+}
+
 pub struct EntityBuilder {
-    entity: UEntity
+    entity: UEntity,
+    id_set: bool,
+    name_set: bool,
+    version_major_set: bool,
 }
 
 impl EntityBuilder {
-    pub fn add_name(self, name: &str) -> UriBuilder {
-        EntityBuilder { entity: () }
+
+    pub fn new() -> EntityBuilder {
+        EntityBuilder {
+            entity: UEntity {
+                name: String::new(),
+                id: None,
+                version_major: None,
+                version_minor: None,
+            },
+            id_set: false,
+            name_set: false,
+            version_major_set: false,
+        }
     }
 
-    pub fn add_version(self) -> UriBuilder {
-        EntityBuilder { entity: () }
+    pub fn add_name(mut self, name: &str) -> Self {
+        self.entity.name = name.to_string();
+        self.name_set = true;
+        self
     }
 
-    pub fn add_id(self, id: u16) -> UriBuilder {
-        EntityBuilder { entity: () }
+    pub fn add_version_minor(mut self, minor: u32) -> Self {
+        self.entity.version_minor = Some(minor);
+        self
     }
 
-    pub fn build(self) -> UEntity {
-        self.entity
+    pub fn add_version_major(mut self, major: u32) -> Self {
+        self.entity.version_major = Some(major);
+        self.version_major_set = true;
+        self
+    }
+
+    pub fn add_id(mut self, id: u32) -> Self {
+        self.entity.id = Some(id);
+        self.id_set = true;
+        self
+    }
+
+    pub fn build(self) -> EntityResolution {
+        match (self.id_set, self.name_set, self.version_major_set) {
+            (true, true, true) => EntityResolution::Resolved(self.entity),
+            (true, false, true) => EntityResolution::MicroUri(self.entity),
+            (false, true, true) => EntityResolution::LongUri(self.entity),
+            _ => EntityResolution::Incomplete(self.entity),
+        }
     }
 }
